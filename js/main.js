76c2eaae914e37d86f31bc141816d3970011adf7
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-01-07 12:13:36 +0300

    add code for activating accordion

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function addAccordion() {
+    const titles = document.querySelectorAll('.label');
+titles.forEach(function(title) {
+    title.addEventListener('click', toggleActiveClass);
+})
+}
+function toggleActiveClass() {
+    this.classList.toggle('active');
+}
+addAccordion();
