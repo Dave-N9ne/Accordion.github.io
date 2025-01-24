8766d1982aebf8de8a9438aa45e6c08965afdd5b
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-01-24 21:28:47 +0300

    add spaces between functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 function addAccordion() {
     const titles = document.querySelectorAll('.label');
-titles.forEach(function(title) {
+    titles.forEach(function(title) {
     title.addEventListener('click', toggleActiveClass);
 })
 }
+
 function toggleActiveClass() {
     this.classList.toggle('active');
 }
+
 addAccordion();
